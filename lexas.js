e37937cf383f3d5f58e1b86b82bccddd534e8616
hyrious <hyrious@outlook.com>
2017-09-25 00:54:13 +0800

    .

--- lexas.js
+++ lexas.js
@@ -1,58 +1,85 @@
 'use strict'
 
+Array.prototype.last = function(rindex = 1) {
+  return this[this.length - rindex];
+};
+
+const TOKEN = {
+  OPERAND: Symbol(),
+  OPERATOR: Symbol(),
+  SKIP: Symbol(),
+  LEFT: Symbol(),
+  RIGHT: Symbol(),
+  BINARY: Symbol(),
+  BRACKET: Symbol(),
+}
+
+const EOP = { type: TOKEN.OPERATOR, symbol: '$', precedence: -1, fixity: 'left' }
 class Lexas {
-  constructor({ tokens, rules }) {
-    this.tokens = tokens
-    this.grammar = {}
-    for (let [id, pattern] of rules) {
-      if (!(id in this.grammar)) this.grammar[id] = []
-      this.grammar[id].push(pattern)
-    }
-    for (let id in this.grammar) {
-      let rules = this.grammar[id]
-      let left = [], right = []
-      for (let rule of rules) {
-        if (rule[0] === id)
-          left.push(rule)
-        else
-          right.push(rule)
-      }
-      if (left.length) {
-        right.forEach(n => n.push(`_${id}`))
-        this.grammar[id] = right
-        left.forEach(l => (l.shift(), l.push(`_${id}`)))
-        this.grammar[`_${id}`] = left
-      }
-    }
+  constructor(rules) {
+    this.stack = [{ oprand: [], op: [] }]
+    this.rules = rules
   }
-  lex(str, pos = 0, patterns = this.grammar[this.tokens[0]], id = this.tokens[0], token = this.tokens[0]) {
-    let child = [], length = 0
-    for (let rule of patterns) {
-      for (let elem of rule) {
-        let ret
-        if (elem in this.grammar)
-          ret = this.lex(str, pos + length, this.grammar[elem], elem, this.tokens.includes(elem) ? elem : token)
-        if (str.startsWith(elem, pos))
-          ret = { token, child: elem, length: elem.length }
-        if (ret && ret.length) {
-          child.push(ret)
-          length += ret.length
-        } else break
+  lex() {
+    if (!this.str) return null
+    let m, ret
+    for (const [rule, out] of this.rules) {
+      ret = out
+      if (typeof rule === 'string') {
+        if (this.str.startsWith(rule))
+          m = rule
+      } else if (m = this.str.match(rule))
+        m = m[0]
+      if (m) {
+        this.str = this.str.substring(m.length)
+        break
       }
     }
-    return { token, child, length }
-  }
-  flattenLex(...args) {
-    let ret = []
-    let lexed = this.lex(...args)
-    const _dfs = node => {
-      if (typeof node.child === 'string') ret.push(node)
-      else node.child.forEach(_dfs)
-    }
-    _dfs(lexed)
+    if (typeof ret === 'function') ret = ret(m)
     return ret
   }
-  parse(flexed) {
-    
+  parse(str) {
+    this.str = str
+    let x
+    while (x = this.lex()) this.doToken(x)
+    this.doStack(EOP)
+    return this.stack.last().oprand.last()
+  }
+  compare(a, b) {
+    if (a.precedence > b.precedence) return true
+    if (a.precedence < b.precedence) return false
+    if (a.fixity !== b.fixity) {
+      throw new Error(`Can't compose ${a.symbol} with ${b.symbol}.`)
+      return
+    }
+    return a.fixity == 'left'
+  }
+  doStack(x) {
+    while (this.stack.last().op.length && this.compare(this.stack.last().op.last(), x)) {
+      const rhs = this.stack.last().oprand.pop()
+      const lhs = this.stack.last().oprand.pop()
+      const op = this.stack.last().op.pop()
+      this.stack.last().oprand.push({ type: TOKEN.BINARY, op, lhs, rhs })
+    }
+  }
+  doToken(x) {
+    switch (x.type) {
+      case TOKEN.OPERAND: this.stack.last().oprand.push(x); break
+      case TOKEN.LEFT: this.stack.push({ oprand: [], op: [], match: x.match, value: x.value }); break
+      case TOKEN.RIGHT: {
+        if (x.value !== this.stack.last().match)
+          throw new Error(`Mismatched bracket, expected ${this.stack.last().match}, got ${x.value}`)
+        else {
+          this.doStack(EOP)
+          let last = this.stack.pop()
+          this.stack.last().oprand.push({ type: TOKEN.BRACKET, symbol: last.value, node: last.oprand.last() })
+        }
+        break
+      }
+      case TOKEN.OPERATOR: {
+        this.doStack(x)
+        this.stack.last().op.push(x)
+      }
+    }
   }
 }
